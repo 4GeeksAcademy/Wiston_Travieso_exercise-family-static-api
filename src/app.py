95c2805f4a956949c44f2782bedd87012b14ce75
WistonTravieso <wistontravieso@gmail.com>
2024-11-21 23:37:30 +0000

    [Error en GET member no levanta Servidor]

--- src/app.py
+++ src/app.py
@@ -30,13 +30,36 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+@app.route('member/<int:id>', methods=['GET']) 
+def get_member(id):
+      member = jackson_family.get_member(id)
+      return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def post_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información del body'}), 400
+    if "first_name" not in body:
+        return jsonify({'msg': "El campo first_name es obligatorio"}), 400
+    if "age" not in body:
+        return jsonify({'msg': "El campo age es obligatorio"}), 400
+    if "lucky_numbers" not in body:
+        return jsonify({'msg': "El campo lucky_numers es obligatorio"}), 400
+    new_member = {
+             'id': jackson_family._generateId(),
+             'first_name':body['first_name'],
+             'last_name':jackson_family.last_name,
+             'age':body['age'],
+             'luky':body['lucky_numbers'],
+            }
+    members = jackson_family.add_member(new_member)
+    return jsonify({'msg': "OK", 'members':members })
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
